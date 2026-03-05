736c7a960edf125ce2f9d98661c060cc0a75e82e
vishal-miriyala-2006 <vishalmiriyala900@gmail.com>
2026-03-06 00:51:58 +0530

    added upload artifact

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,10 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.vitest
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
